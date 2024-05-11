b5414116ef8db79ce545c821453425f2a1fb5a60
src_resources <src.res.211@gmail.com>
2024-05-12 01:05:49 +0800

    Move application logic into app.rs
    Signed-off-by: src_resources <src.res.211@gmail.com>

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use std::future::Future;
+use std::ptr;
 use axum::response::Html;
 use axum::Router;
 use axum::routing::{get, post};
@@ -7,6 +8,7 @@ use axum::extract;
 use axum::response::IntoResponse;
 use axum::http::StatusCode;
 use thiserror::Error;
+use tokio::signal;
 use crate::database::Database;
 use crate::messaging::{PackType, TrainStationScheduleUpdate};
 
@@ -57,11 +59,34 @@ impl App {
     }
 }
 
-pub async fn init_app(config_file_path: String) -> Result<App, AppInitError> {
+static mut APP_PTR: *mut App = ptr::null_mut();
+
+pub async fn init() -> Result<(), AppInitError> {
+    let mut app = init_instance("run/config.json".to_string()).await?;
+    unsafe {
+        APP_PTR = &mut app;
+    }
+
+    Ok(())
+}
+
+async fn init_instance(config_file_path: String) -> Result<App, AppInitError> {
     let database = Database::load_from_file(config_file_path).ok_or(
         AppInitError::Database("Error loading database.".to_string()))?;
     let app = App::new(database);
     Ok(app)
+}
+
+pub async fn run() -> Result<(), Box<dyn Error>> {
+    app_instance().run(on_app_shutdown()).await
+}
+
+pub fn app_instance() -> &'static App {
+    unsafe { &*APP_PTR }
+}
+
+pub fn app_instance_mut() -> &'static mut App {
+    unsafe { &mut *APP_PTR }
 }
 
 async fn handler() -> Html<String> {
@@ -127,3 +152,32 @@ async fn handler_api(
     }
 }
 
+async fn on_app_shutdown() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+        let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+        let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => do_app_shutdown().await,
+        _ = terminate => do_app_shutdown().await,
+    }
+}
+
+async fn do_app_shutdown() {
+    unsafe {
+        app_instance().shutdown("run/config.json".to_string()).await
+    }
+}
