989adafa349c8b27204a239610b25b879cf48219
src_resources <src.res.211@gmail.com>
2024-05-15 22:53:12 +0800

    Migrate web content generating to askama
    Signed-off-by: src_resources <src.res.211@gmail.com>

--- src/web.rs
+++ src/web.rs
@@ -1,35 +1,35 @@
+use askama::Template;
 use crate::app;
 
-pub fn generate_homepage() -> String {
-    use rtml::*;
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate {
+    train_stations: Vec<(String, Vec<(String, String, String)>)>
+}
 
-    let mut body_str = String::from("<ul>");
+pub fn generate_homepage() -> String {
+    let mut temp_train_stations: Vec<(String, Vec<(String, String, String)>)> = Vec::new();
+
     let train_stations = app::app_instance().database().train_stations();
     for train_station in train_stations.iter() {
-        body_str.push_str(format!("<li>{}<ul>", train_station.name()).as_str());
-        for (index, entry) in train_station.schedule().entries().iter().enumerate() {
-            body_str.push_str(format!(
-                "<li>Schedule {}<ul><li>Time left: {}</li><li>Train name: {}</li><li>Train destination: {}</li></ul></li>",
-                index,
-                entry.time_left().unwrap_or_else(|| 0),
-                entry.train_name(),
-                entry.train_destination()).as_str());
+        let mut temp_schedules: Vec<(String, String, String)> = Vec::new();
+        for entry in train_station.schedule().entries().iter() {
+            temp_schedules.push((
+                format!("{}", entry.time_left().unwrap_or(0)),
+                entry.train_name().to_string(),
+                entry.train_destination().to_string()
+            ));
         }
-        body_str.push_str("</ul></li>")
+        temp_train_stations.push((
+            train_station.name(),
+            temp_schedules
+        ));
     }
-    body_str.push_str("</ul>");
 
-    // Use the macros to generate some HTML
-    let mut document: String = html!{
-        .lang = "en",
-            head!{
-                title!{
-                    "Title of the document"
-                }
-            },
-            body!("$$$TEMPLATE$$$")
-    }.render();
-    document = document.replace("$$$TEMPLATE$$$", body_str.as_str());
+    let temp = IndexTemplate {
+        train_stations: temp_train_stations
+    };
+    let document = temp.render().unwrap();
 
     document
 }
